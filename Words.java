c141b1703d40854bf2af9c6f27c55c381f755aa7
Tanya Kadiyala <123403349+mvAA77@users.noreply.github.com>
2025-09-14 22:09:03 -0400

    Update Words.java

--- Words.java
+++ Words.java
@@ -1,50 +1,101 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Words {
 
-    private Stack word;
+    private Stack words;
     Random random = new Random();
-    private int rand;
     private String[] themes = {"Pets", "Pool Party Food", "Birds", "Colors", "Vacation Spots", "Instruments"};
     private String theme;
 
-
     public Words() {
-        rand = (int)(Math.random() * ((5) + 1)) + 5;
-        word = new Stack(rand);
-
-        int randTheme = random.nextInt(5);
+        words = new Stack(5);
+        int randTheme = random.nextInt(themes.length);
         theme = themes[randTheme];
-
     }
 
-    public int getSpaces() {
-        return rand;
+    public int getLength() {
+        return words.size();
     }
 
     public String getTheme() {
         return theme;
     }
 
-    public Stack getWord() {
-        String thisWord = "";
-        for (int i = 0, i < word.size(); i++) {
-            thisWord += word.pop();
-        }
+    public Object getLastWord() {
 
-        for (int i = thisWord.length()-1; i >=0; i--) {
-            word.push(thisWord.charAt(i));
+        if (words.isEmpty()) {
+            return "";
+        } else {
+            return words.peek();
         }
-        String officialWord = "";
-         for (int i = thisWord.length() - 1; i >= 0; i--) {
-            officialWord += thisWord.charAt(i);
-        }
-        return officialWord;
-
     }
 
-    
+    public void deletePrevious() {
+        if (!words.isEmpty()) {
+            words.pop();
+        }
+    }
+
+    public void addWord(String word) {
+        words.push(word);
+    }
+
     public static void main(String[] args) {
-        Stack stackie = new Stack(10);
+        Scanner input = new Scanner(System.in);
+        Words wordle = new Words();
+        System.out.println("Welcome to Theme Words!");
+        System.out.println("This is your theme: " + wordle.getTheme());
+
+        System.out.println("Please enter a word that fits this theme.");
+        System.out.println("Next, input another word that starts with the same letter the previous word ended in");
+        System.out.println("Press # if you can't think of any new words!");
+        System.out.println("If you want to undo your input, please press: ! ");
+        System.out.println("Good Luck!");
+        
+        // Get the first word
+        System.out.print("Word: ");
+        String userWord = input.nextLine().toLowerCase();
+        wordle.addWord(userWord);
+
+        while (true) {
+            System.out.print("Great! Please enter another word: ");
+            userWord = input.nextLine().toLowerCase();
+    
+            if (userWord.equals("#")) {
+                break;
+            }
+    
+            if (userWord.equals("!")) {
+                wordle.deletePrevious();
+                if (wordle.getLength() > 0) {
+                    System.out.println("Undo successful. Last word was: " + wordle.getLastWord());
+                } else {
+                    System.out.println("No words to undo. Please enter a new word.");
+                }
+                continue;
+            }
+            
+            // Check if the word fits the theme (simple check)
+            String lastWord = String.valueOf(wordle.getLastWord());
+            if (lastWord.isEmpty()) {
+                wordle.addWord(userWord);
+                continue;
+            }
+            
+            char lastCharPrevious = lastWord.charAt(lastWord.length() - 1);
+            char firstCharCurrent = userWord.charAt(0);
+    
+            if (lastCharPrevious == firstCharCurrent) {
+                wordle.addWord(userWord);
+                System.out.println("Good job! The word '" + userWord + "' was added.");
+            } else {
+                System.out.println("Word doesn't match! Last letter was: " + lastCharPrevious);
+                System.out.println("Try again or press # to end the game.");
+            }
+        }
+
+        System.out.println("You're Done! Your score is: " + wordle.getLength());
+        input.close();
     }
 }
